29c5d60dc931132dcc5e60ed7d00d8c3beb76f07
nurs <nursultanzhantuar@gmail.com>
2024-04-17 20:16:59 +0500

    Fixed the moving animation and changed fps to 60

--- game/spriteclasses.py
+++ game/spriteclasses.py
@@ -26,35 +26,71 @@ class Player(pygame.sprite.Sprite):
             pygame.image.load('images/sprites/Boy/back/' +x) for x in os.listdir('images/sprites/Boy/back')
         ]
 
-        self.cur_image = self.walk_face_images[0]
-        self.rect = self.cur_image.get_rect(center = pos)
+        self.default_image = self.walk_face_images[0]
+        self.rect = self.default_image.get_rect(center = pos)
 
-        self.speed = 5
+        self.speed = 3
 
-        self.moving = True
-        self.left = True
-        self.right = False
-        self.walk_count = 0
+        self.moving = {
+                "left" : False,
+                "right" : False,
+                "face" : False,
+                "back" : False
+        }
+
+        self.walk_count = 0 
         #self.up
         #self.down
 
 
     def move(self, pressed):
-        if not self.moving:
-            return
         if pressed[pygame.K_UP]:
-            self.rect.y = max(self.rect.y - 5, 0 + 150)
-            self.cur_image = self.walk_face_images[self.walk_count]
+            for key in self.moving.keys():
+                self.moving[key] = False
+            self.moving["back"] = True
+            self.rect.y = max(self.rect.y - self.speed, 0 + 150)
+
         elif pressed[pygame.K_DOWN]:
-            self.rect.y = min(self.rect.y + 5, self.screen.get_height()-self.rect.height)
+            for key in self.moving.keys():
+                self.moving[key] = False
+            self.moving["face"] = True
+            self.rect.y = min(self.rect.y + self.speed, self.screen.get_height()-self.rect.height)
+
         elif pressed[pygame.K_LEFT]:
-            self.rect.x -= 5
+            for key in self.moving.keys():
+                self.moving[key] = False
+            self.moving["left"] = True
+            self.rect.x -= self.speed
+
         elif pressed[pygame.K_RIGHT]:
-            self.rect.x += 5
+            for key in self.moving.keys():
+                self.moving[key] = False
+            self.moving["right"] = True
+            self.rect.x += self.speed
+        else:
+            self.moving["right"] = False
+            self.moving["left"] = False
+            self.walk_count = 0
 
 
     def blit(self):
-        self.screen.blit(self.cur_image, self.rect)
+        if self.walk_count + 1 >= 60:
+            self.walk_count = 0 
+        
+        if self.moving["left"]:
+            self.screen.blit(self.walk_left_images[self.walk_count // 12], (self.rect.x, self.rect.y))
+            self.walk_count += 1
+        elif self.moving["right"]:
+            self.screen.blit(self.walk_right_images[self.walk_count // 12], (self.rect.x, self.rect.y))
+            self.walk_count += 1
+        elif self.moving["face"]:
+            self.screen.blit(self.walk_face_images[self.walk_count // 20], (self.rect.x, self.rect.y))
+            self.walk_count += 1
+        elif self.moving["back"]:
+            self.screen.blit(self.walk_back_images[self.walk_count // 20], (self.rect.x, self.rect.y))
+            self.walk_count += 1
+        else:
+            self.screen.blit(self.default_image, (self.rect.x, self.rect.y))
 
 
 class Wall:
--- game/test[nurs].py
+++ game/test[nurs].py
@@ -15,38 +15,12 @@ notebook = Object(1, (195, 280, 200, 120), (255,255,255))
 
 
 
-#isJump = False
-#jumpCount = 5
-
-
-# animCount = 0
-
-# def drawWindow():
-#     global animCount, notebook
-#     win.blit(bg, (0, 0))
-
-#     if animCount + 1 >= 30:
-#         animCount = 0
-
-#     if left:
-#         win.blit(walkLeft[animCount // 5], (x, y))
-#         animCount += 1
-#     elif right:
-#         win.blit(walkRight[animCount // 5], (x, y))
-#         animCount += 1
-#     else:
-#         win.blit(playerStand, (x, y))
-
-
-    # notebook.interaction(playerStand.get_rect(), win, keys)
-    # pygame.display.update()
-
 
 
 run = True
 while run:
     screen.blit(bg, (0,0))
-    clock.tick(30)
+    clock.tick(60)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -58,18 +32,7 @@ while run:
     player.move(keys)
     player.blit()
     notebook.interaction(player, screen, keys)
+    screen.blit()
 
-    #if not(isJump):
-        #if keys[pygame.K_SPACE]:
-            #isJump = True
-    #else:
-        #if jumpCount >= -5:
-            #if jumpCount < 0:
-               # y += (jumpCount**2)/2
-           # else:
-               # y -= (jumpCount**2)/2
-            #jumpCount -= 1
-        #else:
-            #isJump = False
-           # jumpCount = 5
+
     pygame.display.update()
