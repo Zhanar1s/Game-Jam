1183fdb24819e338549ede070b85ae59e378bde2
Zhanarys <admin>
2024-04-19 04:30:19 +0500

    adding rooms

--- game/test[zhans].py
+++ game/test[zhans].py
@@ -1,154 +1,107 @@
 import pygame
 import time
+from spriteclasses_testing import Player, Door, Wall
+from interaction import Interactable
+from lighting import Dim, Light
+from soundbar import sfx
+from menu import *
+
 pygame.init()
-win = pygame.display.set_mode((1200, 600))
-
-walkRight = [
-	pygame.transform.scale(pygame.image.load('images/sprites/Boy/right/1.png'),(170,201)),
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/right/2.png'),(170,201)),
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/right/3.png'),(170, 201)),
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/right/4.png'),(170, 201)),
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/right/5.png'), (170, 201)),
-
-]
-
-walkUp = [
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/back/1.png'),(125,201)),
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/back/2.png'),(125,201)),
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/back/3.png'),(125, 201))
-]
-
-walkLeft = [
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/left/1.png'),(170,201)),
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/left/2.png'),(170,201)),
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/left/3.png'),(170, 201)),
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/left/4.png'),(170, 201)),
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/left/5.png'), (170, 201)),
-
-]
-
-walkDown = [
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/face/1.png'),(125,201)),
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/face/2.png'),(125,201)),
-    pygame.transform.scale(pygame.image.load('images/sprites/Boy/face/3.png'),(125, 201))
-]
+screen = pygame.display.set_mode((1280, 720))
 
 
-
-bg = pygame.transform.scale(pygame.image.load('images/background.png'), (1200, 600))
-bg1 = pygame.transform.scale(pygame.image.load('images/background.png'), (1200, 600))
-playerStand = pygame.transform.scale(pygame.image.load('images/sprites/Boy/face/1.png'), (125, 201))
-
-door = pygame.transform.scale(pygame.image.load('images/door/door (1).png'), (220, 285))
-win.blit(door, (500, 500))
-door_open = pygame.transform.scale(pygame.image.load('images/door/door (2).png'), (220, 285))
-
-in_door_open = False
-
+bg_room1 = pygame.transform.scale(pygame.image.load('images/room/room1.png'), (1280, 720))
+bg_room2 = pygame.transform.scale(pygame.image.load('images/room/room2.png'), (1280, 720))
+bg_room3 = pygame.transform.scale(pygame.image.load('images/room/room3.png'), (1280, 720))
+bg_room4 = pygame.transform.scale(pygame.image.load('images/room/room4.png'), (1280, 720))
+bg_room5 = pygame.transform.scale(pygame.image.load('images/room/room3.png'), (1280, 720))
 
 clock = pygame.time.Clock()
 
+player = Player(screen, (50, 600))
 
-x = 50
-y = 380
-width = 125
-height = 201
-speed = 5
+wall1 = Wall((0, 0), (1280, 200))
+table_border = Wall((240, 280), (250, 80))
+door = Door(screen, (600, 60))
 
-left = False
-right = False
-up = False
-down = False
-animCount = 0
+notebook = Interactable(1, (195, 280, 250, 120), (255, 255, 255), room="room1", item="notebook")
+puddle = Interactable(1, (950, 280, 200, 200), (255, 255, 255), room="room1", item="puddle")
 
-def drawWindow():
-    global in_door_open
-    global animCount
+lantern = Light(screen, (220, 220, 220), 25, (player.rect.x + 97, player.rect.y + 152))
+dim = Dim(screen)
 
+bgm_channel = pygame.mixer.Channel(0)
+sfx_channel = pygame.mixer.Channel(1)
 
-    if in_door_open:
+game_menu(bgm_channel, sfx_channel, "main")
+sfx_channel.play(sfx["ambience"], -1)
 
-        win.blit(bg1, (0, 0))
-        #win.blit(door_open, (900, 100))
+current_room = "room1"
+current_room_bg = bg_room1
 
+def scene1():
+    global current_room, current_room_bg
+    door_open_time = 0
+    run = True
+    while run:
+        screen.blit(current_room_bg, (0, 0))
+        clock.tick(60)
 
-    else:
-        win.blit(bg, (0, 0))
-        win.blit(door, (900, 100))
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_z:
+                    if notebook.rect.colliderect(player.rect):
+                        notebook.enable = True
+                    if puddle.rect.colliderect(player.rect):
+                        puddle.enable = True
+                    if door.rect.colliderect(player.rect) and time.time() - door_open_time > 2:
+                        door.open_door()
+                        door_open_time = time.time()
+                        if current_room == "room1":
+                            current_room = "room2"
+                            current_room_bg = bg_room2
+                            player.rect.x = 50
+                            player.rect.y = 380
+                            door.close_door()
+                        elif current_room == "room2":
+                            current_room = "room3"
+                            current_room_bg = bg_room3
+                            player.rect.x = 50
+                            player.rect.y = 380
+                            door.close_door()
+                        elif current_room == "room3":
+                            current_room = "room4"
+                            current_room_bg = bg_room4
+                            player.rect.x = 50
+                            player.rect.y = 380
+                            door.close_door()
+                        elif current_room == "room4":
+                            current_room = "room5"
+                            current_room_bg = bg_room5
+                            player.rect.x = 50
+                            player.rect.y = 380
+                            door.close_door()
+                if event.key == pygame.K_ESCAPE:
+                    game_menu(bgm_channel, sfx_channel, "pause")
 
-    if animCount + 1 >= 30:
-        animCount = 0
+        keys = pygame.key.get_pressed()
 
-    if left:
-        win.blit(walkLeft[animCount // 6], (x, y))
-        animCount += 1
-    elif right:
-        win.blit(walkRight[animCount // 6], (x, y))
-        animCount += 1
-    elif up:
-        win.blit(walkUp[animCount // 10], (x, y))
-        animCount += 1
-    elif down:
-        win.blit(walkDown[animCount // 10], (x, y))
-        animCount += 1
+        player.move(keys, lantern)
 
+        door.blit()
+        player.blit()
 
+        dim.darken(150)
 
+        lantern.blit((100, 100, 100), size=5)
 
-    else:
-        win.blit(playerStand, (x, y))
+        notebook.interaction(player, screen, keys)
+        puddle.interaction(player, screen, keys)
 
-    pygame.display.update()
+        player.wall_collision(Wall.walls)
 
+        pygame.display.update()
 
-
-run = True
-while run:
-    clock.tick(30)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_e and (x > 900) and (y < 151):
-                win.blit(door_open, (900, 100))
-                pygame.display.update()
-                time.sleep(1)
-                in_door_open = not in_door_open
-                if in_door_open:
-                    x, y = 50, 380
-
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and x > 5:
-        x -= speed
-        left = True
-        right = False
-        up = False
-        down = False
-    elif keys[pygame.K_RIGHT] and x < 1200 - width - 5:
-        x += speed
-        left = False
-        right = True
-        up = False
-        down = False
-    elif keys[pygame.K_UP] and y > 150:
-        y -= speed
-        left = False
-        right = False
-        up = True
-        down = False
-    elif keys[pygame.K_DOWN] and y < 600 - height - 5:
-        y += speed
-        left = False
-        right = False
-        up = False
-        down = True
-
-    else:
-        left = False
-        right = False
-        animCount = 0
-
-
-    drawWindow()
+scene1()
